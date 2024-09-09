bc1f54f34fc2371d61c36bc262a46d6133a71dc6
Thiago Vivas de Araujo <thiago.vivas@outlook.com>
2024-09-09 22:24:08 +0100

    Final commit

---
+++ OrientacaoObjetos/Carro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjetos
+{
+    // Definindo a classe Carro
+    public class Carro
+    {
+        // Atributos (propriedades) do carro
+        public string Cor { get; set; }
+        public string Modelo { get; set; }
+        public int Velocidade { get; set; }
+
+        // Construtor para inicializar o carro
+        public Carro(string cor, string modelo, int velocidade)
+        {
+            Cor = cor;
+            Modelo = modelo;
+            Velocidade = velocidade;
+        }
+
+        // Método para acelerar
+        public void Acelerar()
+        {
+            Velocidade += 10;
+            Console.WriteLine("O carro está acelerando. Velocidade atual: " + Velocidade + " km/h");
+        }
+
+        // Método para frear
+        public void Frear()
+        {
+            Velocidade -= 10;
+            Console.WriteLine("O carro está freando. Velocidade atual: " + Velocidade + " km/h");
+        }
+    }
+
+}
